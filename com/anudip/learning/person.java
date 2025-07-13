883c29d98ee89c2325f777e62e5f3255c0fd1bd0
pratiksha bhosale <143013153+jaya9850@users.noreply.github.com>
2025-07-14 00:27:02 +0530

    Create person.java

---
+++ com/anudip/learning/person.java
@@ -0,0 +1,11 @@
+package com.anudip.learning;
+
+public class Person {
+    String name = "Pratiksha";
+    int age = 22;
+    int salary = 25000;
+
+    public static void main(String[] args) {
+        System.out.println("Test Successful");
+    }
+}
