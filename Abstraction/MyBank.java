dd56e4948dd038932d5ece31dcd1b54a3dc6d316
pratiksha bhosale <143013153+jaya9850@users.noreply.github.com>
2025-07-22 13:58:09 +0530

    Create MyBank.java

---
+++ Abstraction/MyBank.java
@@ -0,0 +1,13 @@
+public void lo 
+
+
+Package Abstract_demo
+public  abstract class mybank{
+  int acN0 =121,PIn=111; 
+
+  Scanner sc= new Scanner(System.in);
+public void login(){
+     
+}
+    
+}
