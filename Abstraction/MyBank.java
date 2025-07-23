46868e7eb33234979813f7e930fd3be67625d435
pratiksha bhosale <143013153+jaya9850@users.noreply.github.com>
2025-07-23 12:38:56 +0530

    Update MyBank.java

--- Abstraction/MyBank.java
+++ Abstraction/MyBank.java
@@ -1,13 +1,40 @@
-public void lo 
 
+import java.util.Scanner;
 
-Package Abstract_demo
-public  abstract class mybank{
-  int acN0 =121,PIn=111; 
+// Abstract class
+abstract class MyBank {
+    int acNo = 121;
+    int pin = 111;
 
-  Scanner sc= new Scanner(System.in);
-public void login(){
-     
+    Scanner sc = new Scanner(System.in);
+
+    // Abstract method
+    public abstract void login();
 }
-    
+
+// Subclass implementing abstract method
+class BankUser extends MyBank {
+
+    @Override
+    public void login() {
+        System.out.print("Enter Account Number: ");
+        int userAc = sc.nextInt();
+
+        System.out.print("Enter PIN: ");
+        int userPin = sc.nextInt();
+
+        if (userAc == acNo && userPin == pin) {
+            System.out.println("Login Successful ✅");
+        } else {
+            System.out.println("Login Failed ❌");
+        }
+    }
+}
+
+// Main class
+public class Main {
+    public static void main(String[] args) {
+        BankUser user = new BankUser();
+        user.login();
+    }
 }
