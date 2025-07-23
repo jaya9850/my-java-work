bc096de8f102c0d0734735d8a01ec9b11445316c
pratiksha bhosale <143013153+jaya9850@users.noreply.github.com>
2025-07-23 12:57:24 +0530

    Create Polyc.java

---
+++ polymor/Polyc.java
@@ -0,0 +1,15 @@
+Package Poly_demo;
+public class Method_overloding {
+      public static void main(String[] args)
+  {
+     obj.add(1,10);
+    obj.add();
+    
+       
+     
+  }
+  public void add()
+  {
+    SOUT (" imp proceess add n num")
+      
+  }
